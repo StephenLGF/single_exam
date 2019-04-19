1dc8a7f66992ba5bebb57cdab2a6489019a2b1d0
StephenGit <gengfeng_luo@jinbag.com>
2019-04-19 10:12:26 +0800

    hello world

---
+++ exam/src/main/java/com/SpringBootApp.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class SpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
