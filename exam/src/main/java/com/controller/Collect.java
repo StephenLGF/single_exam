efb2a8a0c5886abd6d5b8b7824d4b84efc9e3ed5
StephenGit <gengfeng_luo@jinbag.com>
2019-04-24 15:09:55 +0800

    collection接口测试token的使用

--- exam/src/main/java/com/controller/Collect.java
+++ exam/src/main/java/com/controller/Collect.java
@@ -1,11 +1,15 @@
 package com.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.entity.Collection;
 import com.servie.CollectionService;
+import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
 
 @RestController
 @RequestMapping("api")
@@ -21,8 +25,20 @@ public class Collect {
     }
 
     @PutMapping(value = "/collection/{questionId}")
-    public ResponseEntity createWrongQuestion(@PathVariable Long questionId) {
-        Long userId = 1L;
+    public ResponseEntity createWrongQuestion(@PathVariable Long questionId, @RequestBody String json) {
+        JSONObject body = JSONObject.parseObject(json);
+        String token = body.getString("token");
+        if (token == null || token.isEmpty()) {
+            return new ResponseEntity(null, HttpStatus.UNAUTHORIZED);
+        }
+        Hashids hashids = new Hashids("this is my salt");
+        long[] decodeLong = hashids.decode(token);
+        Long userId = decodeLong[0];
+        Long tokenTime = decodeLong[1];
+        Long now = Instant.now().toEpochMilli();
+        if (now - tokenTime > 1000 * 60 * 60 * 5) {
+            return new ResponseEntity(null, HttpStatus.UNAUTHORIZED);
+        }
         Collection collection = collectionService.addCollection(questionId, userId);
         if (collection != null) {
             return new ResponseEntity(collection, HttpStatus.OK);
